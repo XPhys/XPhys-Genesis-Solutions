bf9fa5924e36bec65f1d2a06815de970e40aff40
XPhysics AbrasaX <xphysics@protonmail.ch>
2015-06-21 02:51:08 +0700

    Renew XPhys.Me

---
+++ XPhys.AngularJSCampus/Scripts/app.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="angular.js" />
+(function () {
+    var gems = [
+        { name: 'Azurite', price: 2.95, description: 'This is description ..', canPurchase: true },
+        { name: 'Pentagonal Gem', price: 5.95, description: 'This is description ..', canPurchase: true }
+    ];
+    var app = angular.module('gemStore', []);
+    app.controller("StoreController", function () {
+        this.products = gems;
+    });
+
+    app.controller("PanelController", function () {
+        this.tab = 1;
+        this.selectTab = function (setTab) {
+            this.tab = setTab;
+        };
+
+        this.isSelected = function (checkTab) {
+            return this.tab === checkTab;
+        };
+    });
+
+})();
+
+
+
---
+++ XPhys.SportStore/Filters/customFilters.js
@@ -0,0 +1,47 @@
+﻿/// <reference path="/Scripts/angular.js" />
+//angular.module("customFilters", [])
+//.filter("unique", function () {
+//    return function (data, propertyName) {
+//        if (angular.isArray(data) && angular.isString(propertyName)) {
+//            var results = [];
+//            var keys = {};
+//            for (var i = 0; i < data.length; i++) {
+//                var val = data[i][propertyName];
+//                if (angular.isUndefined(keys[val])) {
+//                    keys[val] = true;
+//                    results.push(val);
+//                }
+//            }
+//            return results;
+//        } else {
+//            return data;
+//        }
+//    }
+//});
+
+
+angular.module("customFilters", [])
+.filter("unique", function () {
+    return function (data, propertyName) {
+        if(angular.isArray(data) && angular.isString(propertyName))
+        {
+            var result = [];
+            var keys = {};
+            for(var i = 0 ; i < data.length ; i++)
+            {
+                var val = data[i][propertyName];
+                if(angular.isUndefined(keys[val]))
+                {
+
+                }
+            }
+            return result;
+        }
+        else
+        {
+            return data;
+        }
+    }
+});
+
+
